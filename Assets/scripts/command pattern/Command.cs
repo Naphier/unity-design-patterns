e4e14025017bf14f3eecb1c924fdadad0a221f49
Sean Mann <naplandgames@gmail.com>
2015-10-01 17:12:49 -0400

    Commit command pattern example

---
+++ Assets/scripts/command pattern/Command.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// The 'Command' abstract class that we will inherit from
+/// </summary>
+abstract class Command
+{
+    public abstract void Execute();
+    public abstract void UnExecute();
+}
