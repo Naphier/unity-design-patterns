e7d6907f04de41fbc2f050bcb66ea71374238b45
Derek Mattson <derektmattson@gmail.com>
2016-12-04 17:31:59 -0800

    Added singleton pattern, with example.

---
+++ Assets/singleton/structure/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace NG.Patterns.Structure.Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        //The instance of the class type that has been specified
+        private static T _instance;
+
+        //The public accessor for other classes to access the Singleton. Simply by typing the <ClassName>.Instance
+        public static T Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public virtual void Start()
+        {
+            //Check to make sure there isn't already an instance of this class.
+            if (_instance == null)
+            {
+                //If there isn't find the instance and set it.
+                _instance = FindObjectOfType<T>();
+            }
+            else
+            {
+                //If there is already an instance of this class, destroy the object we just created.
+                print("Attempted to spawn another singleton object, destroying new instance of " + gameObject.ToString());
+                Destroy(gameObject);
+            }
+        }
+    }
+}
