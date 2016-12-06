d26af8f66c0a5ed0b7cbe92824d82aac50afab7f
Sean Mann <naplandgames@gmail.com>
2016-12-06 00:31:03 -0500

    Fixing indents

--- Assets/singleton/example/Scripts/ManagerSpawner.cs
+++ Assets/singleton/example/Scripts/ManagerSpawner.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class ManagerSpawner : MonoBehaviour
 {
-    public GameObject Manager;
-    public Text DebugText;
+	public GameObject Manager;
+	public Text DebugText;
 
 	// Update is called once per frame
 	void Update ()
-    {
-	    if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(Manager);
-            DebugText.text = "If you press space again, a warning will tell you the new instance failed, because it is a singleton.\n\nThis is great for scripts like Managers, that you only want to exist once, and to easily be accessed by other scripts.";
-        }	
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(Manager);
+			DebugText.text = "If you press space again, a warning will tell you the new instance failed, because it is a singleton.\n\nThis is great for scripts like Managers, that you only want to exist once, and to easily be accessed by other scripts.";
+		}	
 	}
 }
